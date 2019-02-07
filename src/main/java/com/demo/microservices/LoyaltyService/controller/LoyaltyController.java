4c5251de2ae3c8f96b04f16e55052a92c01dcd18
Mukesh <mail2mkumar@gmail.com>
2019-02-07 14:35:05 -0600

    Added sleuth

--- src/main/java/com/demo/microservices/LoyaltyService/controller/LoyaltyController.java
+++ src/main/java/com/demo/microservices/LoyaltyService/controller/LoyaltyController.java
@@ -1,31 +1,36 @@
 package com.demo.microservices.LoyaltyService.controller;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoyaltyController {
-	
+
+	private static Logger LOGGER = LoggerFactory.getLogger(LoyaltyController.class);
+
 	Map<String, String> pointsMap = new HashMap<>();
-	
+
 	@GetMapping("/points/{userId}")
-	public String getLoyaltyPoints(@PathVariable String userId){
-		
-		String id = userId ;
-		
-		pointsMap.put("1000","80");
-		pointsMap.put("1001","81");
-		pointsMap.put("1002","82");
-		pointsMap.put("1003","83");
-		
+	public String getLoyaltyPoints(@PathVariable String userId) {
+
+		LOGGER.info("Enter LoyaltyController " + userId);
+
+		String id = userId;
+
+		pointsMap.put("1000", "80");
+		pointsMap.put("1001", "81");
+		pointsMap.put("1002", "82");
+		pointsMap.put("1003", "83");
+
+		LOGGER.info("Exit LoyaltyController " + userId);
+
 		return pointsMap.get(id);
 	}
-	
-	
-	
+
 }
